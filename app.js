bc16232d6d209537b2ae7758ffd2c9d1b142a220
nwmason <[200~174368433+nwmason@users.noreply.github.com>
2024-08-01 14:52:17 -0400

    computer selection fade out implemented

--- app.js
+++ app.js
@@ -11,32 +11,67 @@ function getComputerChoice() {
     }
 }
 
-//  UI makes this code redundant, commenting out in case it's needed later in development
-// function getPlayerChoice() {
-//     let choice = prompt('Rock, Paper, or Scissors?').toLowerCase().trim()
+function incrementPlayerScore(playerScore) {
+    console.log("score incremented")
+    const scoreNumber = document.querySelector("#playerScore");
+    scoreNumber.textContent = ++playerScore;
 
-//     if (choice.includes('rock')) {
-//         return choice
-//     } else if (choice.includes('scissors')) {
-//         return choice
-//     } else if (choice.includes('paper')) {
-//         return choice
-//     } else {
-//         return choice
-//     }
-// }
+    const computerContainer = document.querySelector(".computerSelection");
+    const computerSelector = document.querySelector(".computerSelector");
+    const resultText = document.querySelector(".resultText");
 
-function incrementPlayerScore() {
-
+    computerContainer.setAttribute("id", "fadeOut");
+    resultText.setAttribute("id", "fadeOut");
+    computerSelector.setAttribute("id", "fadeOut");
+    resultText.addEventListener("transitionend", () => {
+        computerContainer.remove();
+        computerSelector.remove();
+        resultText.remove();
+        createChoiceScreen();
+    }, { once: true });
 }
 
-function incrementComputerScore() {
+function incrementComputerScore(computerScore) {
+    console.log("score incremented")
+    const scoreNumber = document.querySelector("#computerScore");
+    scoreNumber.textContent = ++computerScore;
 
+    const computerContainer = document.querySelector(".computerSelection");
+    const computerSelector = document.querySelector(".computerSelector");
+    const resultText = document.querySelector(".resultText");
+
+    computerContainer.setAttribute("id", "fadeOut");
+    resultText.setAttribute("id", "fadeOut");
+    computerSelector.setAttribute("id", "fadeOut");
+    resultText.addEventListener("transitionend", () => {
+        computerContainer.remove();
+        computerSelector.remove();
+        resultText.remove();
+        createChoiceScreen();
+    }, { once: true });
+}
+
+
+// there's no way this is an efficient solution to anything i've done in this project
+// clearly in over my head in how i decided to go about this
+// the challenge will only further my hunger for power
+function createChoiceScreen() {
+    console.log("we've reached the choice screen")
+    // const selectorRock = document.createElement("div");
+    // const selectorPaper = document.createElement("div");
+    // const selectorScissors = document.createElement("div");
+
+
+    // selectorRock.setAttribute("class", ".selector .container");
+    // selectorRock.setAttribute("id", "#rock");
+    // selectorPaper.setAttribute("class", ".selector .container");
+    // selectorPaper.setAttribute("id", "#paper");
+    // selectorScissors.setAttribute("class", ".selector .container");
+    // selectorScissors.setAttribute("id", "#scissors");
 }
 
 //  game logic, determining who is a winner and who is a loser for the round
-function playRound(playerChoice, computerChoice) {
-    computerChoice = getComputerChoice();
+function playRound(playerChoice, computerChoice, playerScore, computerScore) {
     console.log("round started, player chose " + playerChoice + "\ncomputer chose " + computerChoice);
 
     const container = document.querySelector(".content");
@@ -78,7 +113,6 @@ function playRound(playerChoice, computerChoice) {
                     computerSelector.setAttribute("class", "container computerSelector");
                     computerSelector.setAttribute("style", "margin-top: -64px;")
 
-
                     if (computerChoice === "rock") {
                         computerSelectorImg.src = "img/rock.png";
                         console.log("confirming computer chose rock");
@@ -102,56 +136,66 @@ function playRound(playerChoice, computerChoice) {
                     const mainContainer = document.querySelector(".content");
                     const resultText = document.createElement("pre");
                     resultText.setAttribute("class", "resultText");
+                    computerChoice = "scissors"
+                    console.log(document.body.clientHeight);
+
                     computerSelector.addEventListener("transitionend", () => {
                         if (playerChoice === computerChoice) {
+                            
                             resultText.textContent = "the result is a tie\n\nno points awarded";
-
+                            mainContainer.after(resultText);
+                            resultText.setAttribute("id", "fadeIn");
+                            console.log(document.body.clientHeight);
+                        
+                            computerContainer.setAttribute("id", "fadeOut");
+                            resultText.setAttribute("id", "fadeOut");
+                            computerSelector.setAttribute("id", "fadeOut");
+                        
+                            computerSelector.addEventListener("transitionend", () => {
+                                computerContainer.remove();
+                                computerSelector.remove();
+                                resultText.remove();
+                                createChoiceScreen();
+                            });
                         } else if (playerChoice === "rock" && computerChoice === "scissors") {
                             resultText.textContent = "rock beats scissors\n\nwell done!";
-
+                            mainContainer.after(resultText);
+                            resultText.setAttribute("id", "fadeIn");
+    
+                            console.log(document.body.clientHeight);
+                            incrementPlayerScore(playerScore);
                         } else if (playerChoice === "paper" && computerChoice === "rock") {
                             resultText.textContent = "paper beats rock\n\nwell done!";
-
+                            mainContainer.after(resultText);
+                            resultText.setAttribute("id", "fadeIn");
+    
+                            console.log(document.body.clientHeight);
+                            incrementPlayerScore(playerScore);
                         } else if (playerChoice === "scissors" && computerChoice === "paper") {
                             resultText.textContent = "scissors beats paper\n\nwell done!";
-
+                            mainContainer.after(resultText);
+                            resultText.setAttribute("id", "fadeIn");
+    
+                            console.log(document.body.clientHeight);
+                            incrementPlayerScore(playerScore);
                         } else {
-                            resultText.textContent = "the computer has won\n\nthere's always next time";
-
+                            resultText.textContent = "the computer won the round\n\nwomp womp";
+                            mainContainer.after(resultText);
+                            resultText.setAttribute("id", "fadeIn");
+    
+                            console.log(document.body.clientHeight);
+                            incrementComputerScore(computerScore);
                         }
-                        mainContainer.after(resultText);
-                        resultText.setAttribute("id", "fadeIn");
-                    })
+                    }, { once: true })
 
-                });
+                }, { once: true });
             }
-        });
+        }, { once: true });
     };
-
-
-    //  refactor code time
-    // if (playerChoice != 'rock' && playerChoice != 'paper' && playerChoice != 'scissors') {
-    //         alert('You have chosen an incorrect option. \nYou lose this round!')
-    //         return ++computerScore, playerScore
-    //     } else if (playerChoice === computerChoice) {
-    //         alert('This round was a tie! No points awarded for either player.');
-    //         return
-    //     } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-    //         alert('You have won this round!');
-    //         return ++playerScore, computerScore;
-    //     } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-    //         alert('You have won this round!');
-    //         return ++playerScore, computerScore;
-    //     } else if (playerChoice === 'scissors' && computerChoice === 'paper'){
-    //         alert('You have won this round!');
-    //         return ++playerScore, computerScore;
-    //     } else {
-    //         alert('The computer has won this round!');
-    //         return ++computerScore, playerScore;
-    //     }
 }
 
-function playGame() {
+
+function playGame(playerScore, computerScore) {
     console.log("game started")
     const rockSelect = document.querySelector("#rock");
     const paperSelect = document.querySelector("#paper");
@@ -162,16 +206,16 @@ function playGame() {
     scissorSelect.setAttribute("id", "fadeIn");
 
     rockSelect.addEventListener("click", () => {
-        playRound("rock")
-    })
+        playRound("rock", getComputerChoice(), playerScore, computerScore)
+    }, { once: true })
 
     paperSelect.addEventListener("click", () => {
-        playRound("paper")
-    })
+        playRound("paper", getComputerChoice(), playerScore, computerScore)
+    }, { once: true })
 
     scissorSelect.addEventListener("click", () => {
-        playRound("scissors")
-    })
+        playRound("scissors", getComputerChoice(), playerScore, computerScore)
+    }, { once: true })
 }
 
 //  --------------------   End Game Logic   --------------------  //
@@ -186,7 +230,7 @@ startGame.addEventListener("click", () => {
     splashScreen.addEventListener("transitionend", () => {
         splashScreen.remove();
 
-    });
+    }, { once: true });
 
-    playGame();
+    playGame(0, 0);
 });
